8fb67445d277b7da1cb7afdb30ba0d1c52960624
Max Putilov <max.putilov@gmail.com>
2018-04-21 14:46:13 +0300

    Doer.Users registerOrg

---
+++ da-doer-users/API.fs
@@ -0,0 +1,59 @@
+﻿module DA.Doer.Users.API
+
+open DA.FSX.ReaderTask
+open System.Threading.Tasks
+
+type OrgDoc = {    
+    Name: string
+    OwnerEmail: string
+}
+
+type UserDoc = {
+    OrgId: string
+    Role: string
+    FirstName: string
+    MidName: string 
+    LastName: string
+    Email: string
+    Phone: string
+    Ancestors: string seq
+    Avatar: string
+}
+
+type DataAccessTable = 
+    | User of UserDoc
+    | Org of OrgDoc
+
+type DataAccess = {
+    insertDoc: DataAccessTable -> Task<string>
+}
+
+// 
+
+type RegisterUserInfo = {
+    UserId: string
+    OrgId: string
+    Name: string
+    Email: string
+    Password: string
+    Avatar: string
+    Role: string
+}
+
+type TokensResult = {
+    IdToken: string
+    AccessToken: string
+    RefreshToken: string
+}
+
+type RegisterUserResult = {
+    UserId: string
+    Tokens: TokensResult
+}
+
+type Auth = {
+    registerUser: RegisterUserInfo -> Task<RegisterUserResult>
+}
+
+
+type Api<'a> = ReaderTask<DataAccess * Auth, 'a>
---
+++ da-doer-users/RegisterOrg.fs
@@ -0,0 +1,86 @@
+﻿module DA.Doer.Users.RegisterUser
+
+open DA.Doer.Users.API
+open DA.FSX
+
+open ReaderTask
+
+type PersonName = {
+    FirstName: string
+    LastName: string
+    MiddleName: string
+}
+
+type User = {
+    Name: PersonName
+    Phone: string
+    Email: string
+    Avatar: string
+}
+
+type Org = {
+    Name: string
+}
+
+type RegisterOrgInfo = {
+    User: User
+    Org: Org
+    Password: string
+}
+
+let OWNER_ROLE = "Owner"
+
+let info2org (info: RegisterOrgInfo) = 
+    {
+        Name = info.Org.Name
+        OwnerEmail = info.User.Email
+    }
+
+let info2user orgId (info: RegisterOrgInfo) = 
+    {
+        OrgId = orgId
+        Role = OWNER_ROLE
+        FirstName = info.User.Name.FirstName
+        MidName = info.User.Name.MiddleName
+        LastName = info.User.Name.LastName
+        Email = info.User.Email
+        Phone = info.User.Phone
+        Ancestors = []
+        Avatar = info.User.Avatar
+    }
+
+let info2register orgId userId (info: RegisterOrgInfo) = 
+    {
+        UserId = userId
+        OrgId = orgId
+        Name = sprintf "%s %s %s" info.User.Name.LastName info.User.Name.FirstName info.User.Name.MiddleName
+        Email = info.User.Email
+        Password = info.Password
+        Avatar = info.User.Avatar
+        Role = OWNER_ROLE
+    }
+
+type RegOrgApi<'a> = ReaderTask<RegisterOrgInfo * (DataAccess * Auth), 'a>
+
+let insertOrg'                 info = fun (dataAccess, _) -> info |> info2org |> Org |> dataAccess.insertDoc
+
+let insertUser'   orgId        info = fun (dataAccess, _) -> info |> info2user orgId |> User |> dataAccess.insertDoc
+
+let registerUser' orgId userId info = fun (_,       auth) -> info |> info2register orgId userId |> auth.registerUser
+
+//
+
+let insertOrg                : RegOrgApi<string> =             insertOrg' |> Reader.flat
+
+let insertUser   orgId       : RegOrgApi<string> =             insertUser' orgId |> Reader.flat
+
+let registerUser orgId userId: RegOrgApi<RegisterUserResult> = registerUser' userId orgId |> Reader.flat
+        
+let registerOrg (info: RegisterOrgInfo): Api<RegisterUserResult> = fun x ->
+    (readerTask {
+        let! orgId  = insertOrg
+        let! userId = insertUser   orgId
+        let! result = registerUser orgId userId
+        return result
+    }) (info, x)
+    
